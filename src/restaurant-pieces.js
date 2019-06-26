6dc2cde7d9aab6defaa9209105c9d499f1c9ca3e
RJ Salamanca <salamancaronito@gmail.com>
2019-06-26 10:19:50 -0400

    inital commit

---
+++ src/restaurant-pieces.js
@@ -0,0 +1,34 @@
+const genres = ["mediterranean", "american", "mexican"];
+const mediterraneanRestaurants = ["Mediterranean Grill", "Yalla", "Nik's"];
+const medGrillMenu = [
+    "hummus", "tabouli", "dolmas"
+];
+const yallaMenu = [
+    "hummus wrap", "tabouli wrap", "dolmas"
+];
+
+const niksMenu = [
+    "spanakopita", "gyro", "dolmas"
+];
+
+const americanRestaurants = ["Chik fil a", "Mary Mac's Tea Room", "Jersey Mike's"];
+const chickFilAMenu = [
+    "chicken biscuit", "chicken sandwich", "chicken salad"
+];
+const maryMacsMenu = [
+    "biscuit", "fried chicken", "mashed potatoes"
+];
+const jerseyMikesMenu = [
+    "Big Sandwich", "Bigger Sandwich", "Most Biggerest Sandwich"
+];
+
+const mexicanRestaurants = ["Elmyriachi", "Mezcalito's Cantina", "Matador"];
+const elmyriachiMenu = [
+    "tacos", "burritos", "cheese dip"
+];
+const mezcalitosMenu = [
+    "tortas", "tamales", "cheese dip"
+];
+const matadorMenu = [
+    "cheese dip", "cheese dip", "cheese dip"
+];
